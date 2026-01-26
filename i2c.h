b1d642a446fbfbf040d7c0a9b69110af3b47c80a
Admin_233 <58427795+Admin-233@users.noreply.github.com>
2026-01-26 15:29:37 +0800

    add I2C

---
+++ i2c.h
@@ -0,0 +1,25 @@
+#define NULL 0
+
+typedef enum
+{
+	WRITE = 0,
+	READ = 1,
+	UNKNOWN = 2,
+} RWswitch;
+
+typedef enum
+{
+	OK = 0,
+	NACK = 1,
+	TIMEOUT = 2,
+} ERRORstatus;
+
+void I2Cbegin(signed char addr);
+void I2CsetupTransmission(unsigned char addr, RWswitch choice);
+void I2Cwrite(unsigned char data);
+unsigned char I2Cread(void);
+unsigned char I2CstartTransmission(unsigned char sendStop);
+unsigned char I2Cavailable(void);
+void I2ConReceive(void (*userCallback)(void));
+void I2ConRequest(void (*userCallback)(void));
+void I2Cisr(void) __interrupt(24);
